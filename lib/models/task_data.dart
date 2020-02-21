b883f027adc0fa61742f0245f7e64cc3a4aa0cfd
Harsh <harshmacc@gmail.com>
2020-02-21 19:19:29 +0530

    used unmodifiableListView to restrict the modification of list even while being able to access the whole list of tasks in task_data.dart file

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,22 +1,28 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
 
 class TaskData extends ChangeNotifier {
 
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: "buy milk"),
     Task(name: "buy bread"),
     Task(name: "buy eggs"),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   void addTask (String newTaskTitle){
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
 }
